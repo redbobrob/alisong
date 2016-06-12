163ac379b4a20aa52e788149c27cd41747dda8d8
redbobrob <redbobrob@gmail.com>
2016-06-12 14:03:16 -0700

    Add files via upload

---
+++ about.php
@@ -0,0 +1,62 @@
+<?php $thisPage="about"; ?>
+<div class="inner cover">
+  <h1 class="cover-heading">ABOUT</h1>
+  <p class="lead">MAKING MUSIC.</p>
+  <p class="lead">
+    <!-- <a href="#" class="btn btn-lg btn-default">Learn more</a> -->
+  </p>
+</div>
+<div class="container-fluid">
+  <div class="content_BG">
+    <div class="content">
+
+      <!-- // RESUME -->
+      <!-- // EDITABLE -->
+
+      <p style="text-align:left;">
+      <a href="https://lonecrowjubilee.bandcamp.com/releases">https://lonecrowjubilee.bandcamp.com/releases</a>  
+      Alison Jenkins
+      has been a professional musician and
+      theatre artist for the last twenty years. She got started right out of
+      high school, when she spent a year touring Canada with Vancouver Youth
+      Theatre’s musical Canadian Stories. But it was when she picked up the
+      accordion and joined a group of rabble-rousing friends and folk musicians
+      called the Flying Folk Army that things really took off.</br>
+      </br>
+      Over the next few years Alison joined several other bands, including the
+      Latin-inspired Candelaria, Slavic Soul group Zeellia, the short-lived but always
+      fun swing band Bedroom Parrots, and Turkish cabaret sensations Something About
+      Reptiles, where she honed her singing, accordion and clarinet-playing skills.
+      Professional theatre contracts soon followed, including several productions with
+      Chemainus Theatre as a performer and composer.</br>
+      </br>
+      In 2004, a bunch of friends decided to create a play for the Vancouver Fringe
+      Festival. They knew they wanted it to be a musical, and they knew they wanted it
+      to be about a band of musicians from Newfoundland. A surprise hit, The Back
+      Kitchen Release Party went on to win 2 Jessie Richardson Awards, and was picked
+      up for a successful run (and tour) at the Arts Club Theatre. The soundtrack
+      featured several of Alison’s songs, as well as her work as the company’s music
+      director and arranger.</br>
+      </br>
+      But although Alison was achieving some success as a composer and music director,
+      she knew she wanted to do more performing. A 2009 job offer from Theatre Royal
+      in the historic gold rush town of Barkerville, BC was the opportunity she was
+      seeking. From 2010 to 2014 Alison spent at least 4 months of every year treading
+      the boards in Barkerville as an actor, musician and historical interpreter.</br>
+      </br>
+      A truly well-rounded musician, Alison is equally at home performing on stage,
+      recording in the studio, or writing music and lyrics on her laptop (and
+      accordion). She has played music in BC, across Canada, and internationally.  She
+      has twice been nominated for a Jessie Award (Vancouver’s professional theatre
+      awards), and plays many instruments, including accordion, clarinet, tin whistle,
+      trumpet and piano. Alison has also been a prolific songwriter in the last 5
+      years, and hopes to record an album of her own material soon.</br>
+      </br>
+      </p>
+      <p class="lead" style="font-size:10px;">
+      Photograph: J. Lyonns
+      </p>
+<!-- // END EDITABLE -->
+
+    </div>
+  </div>
